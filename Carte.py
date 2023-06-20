ad333c2d0cdb404d16b908a06b582d1926f3dbad
Haplo40 <137066151+Haplo40@users.noreply.github.com>
2023-06-20 16:04:03 +0200

    Création de la matrice Carte

---
+++ Carte.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+from numpy import array,zeros
+import matplotlib
+from matplotlib.pyplot import imshow, show, colorbar
+
+#Récupération de la matrice D de Donnees
+from Donnees import D
+
+Max =np.max(D)
+#print(Max)
+
+Carte=zeros((Max,Max))
+#print(Carte)
