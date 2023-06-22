570574cb8aee50a1bb65ef38025d6f7dc9a62043
Haplo40 <137066151+Haplo40@users.noreply.github.com>
2023-06-22 11:02:49 +0200

    Add files via upload

--- TriAmeliore.py
+++ TriAmeliore.py
@@ -2,6 +2,9 @@
 
 #Tri des pays par nombre de voisin
 
+import numpy as np
+from numpy import array,zeros
+
 #Récupération de la carte :
 from Carte import carte
 
@@ -48,5 +51,18 @@ def key(pays):
     return -len(valeurs_voisines(carte, pays))
 
 Dt.sort(key=key)
-print(Dt)
+
+Dt1=np.array(Dt)
+
+#Remise à  des couleurs des pays :
+for i in range (0,l):
+    xmin=Dt1[i,0]
+    ymin=Dt1[i,1]
+    xmax=Dt1[i,2]
+    ymax=Dt[i,3]
+    pays=[xmin,ymin,xmax,ymax]
+    colorier(carte,pays,0.0)
+
+
+# print(Dt)
     
